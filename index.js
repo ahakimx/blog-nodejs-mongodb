f29009d0d4927d1f125bd1b8d5b5ac3362dfcbca
ahakimz <nisty@localhost.localdomain>
2018-09-23 07:28:23 -0400

    install express-edge

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 const path = require('path')
+
+const expressEdge = require('express-edge')
 const express = require('express')
+
+const mongoose = require('mongoose')
 
 const app = new express()
 
+mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true })
+
 app.use(express.static('public'))
 
-app.get('/', (req, res, next) => {
-    res.sendFile(path.resolve(__dirname, 'public/pages/index.html'));
+app.use(expressEdge)
+
+app.set('views', `${__dirname}/views`)
+
+app.get('/', (req, res) => {
+    res.render('index');
 
 });
 
-app.get('/about', (req, res, next) => {
-    res.sendFile(path.resolve(__dirname, 'public/pages/about.html'));
+app.get('/about', (req, res) => {
+    res.render('about')
 });
 
 app.get('/post', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public/pages/post.html'))
+    res.render('post');
 
 });
 
 app.get('/contact', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public/pages/contact.html'))
+    res.render('contact');
 
 });
 
